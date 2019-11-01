3b6c884b909249ae25624d765e50e0e82910bb30
Jon Cram <webignition@gmail.com>
2019-11-01 10:53:35 +0000

    Use webignition/base-basil-test-case (#6)

--- tests/Functional/WebServerRunnerTest.php
+++ tests/Functional/WebServerRunnerTest.php
@@ -1,4 +1,8 @@
-<?php declare(strict_types=1);
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
 
 namespace webignition\BasePantherTestCase\Tests\Functional;
 
